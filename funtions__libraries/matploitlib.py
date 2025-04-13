55b4ef08fe9780537c6564210b17a85067a168e5
David <david38ing@gmail.com>
2025-04-13 08:37:40 -0600

    Study list and dic comprenhencion

--- funtions__libraries/matploitlib.py
+++ funtions__libraries/matploitlib.py
@@ -14,7 +14,29 @@ notas = [8, 7.5, 6.1, 9.2, 8.8, 1]
 recetas = 1000
 unidades = 15
 
-print(f"La empresa vendio {math.ceil(recetas/unidades)} unidades")
+# print(f"La empresa vendio {math.ceil(recetas/unidades)} unidades")
 
+#------ funcione lambda
+
+
+# nota_1 = int(input("Ingrese la nota del primer examen: "))
+# nota_2 = int(input("Ingrese la nota del segundo examen: "))
+# nota_3 = int(input("Ingrese la nota del tercer examen: "))
+
+promedio = lambda nota_1, nota_2, nota_3: (nota_1 + nota_2 + nota_3) / 3
+
+# print(promedio(nota_1, nota_2, nota_3))
+
+
+# nombres = ["Juan", "Maria", "Pedro", "Ana", "Luis", "Eva"]
+# for i in range(len(nombres)):
+#     print(f"lo que tiene i es: {i}, lo que tiene nombres es: {nombres[i]}")
+
+
+alturas = [1.70, 1.80, 1.65, 1.75, 1.90]
+pesos = [65, 80, 58, 70, 95]
+
+imc = [round((peso / altura**2), 1) for altura, peso in zip(alturas, pesos)]
+print(imc)
 
 
